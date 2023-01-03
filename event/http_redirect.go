075a681f4ca6fc0f892fb5aaf9476d830dbeb2cf
Samuel Stoltenberg <sam@skelouse.com>
2023-01-03 11:10:46 -0600

    Add example and change usage of redirect

--- event/http_redirect.go
+++ event/http_redirect.go
@@ -2,15 +2,33 @@ package event
 
 import (
 	"fmt"
+	"net/http"
 
 	eventSym "github.com/taubyte/go-sdk-symbols/event"
 )
 
-func (e HttpEvent) Redirect(url string, code uint32) error {
-	err := eventSym.EventHttpRedirect(uint32(e), url, code)
+type httpRedirect struct {
+	eventId uint32
+	url     string
+}
+
+func (r httpRedirect) redirect(code uint32) error {
+	err := eventSym.EventHttpRedirect(r.eventId, r.url, code)
 	if err != 0 {
-		return fmt.Errorf("Redirecting HTTP request failed with: %s", err)
+		return fmt.Errorf("redirecting HTTP request failed with: %s", err)
 	}
 
 	return nil
+}
+
+func (e HttpEvent) Redirect(url string) httpRedirect {
+	return httpRedirect{uint32(e), url}
+}
+
+func (r httpRedirect) Temporary() error {
+	return r.redirect(http.StatusTemporaryRedirect)
+}
+
+func (r httpRedirect) Permanent() error {
+	return r.redirect(http.StatusPermanentRedirect)
 }
